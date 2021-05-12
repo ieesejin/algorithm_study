01d756717770acd1486b26e3e7237467795f8593
ieesejin <ieesejin@naver.com>
2021-05-12 16:30:29 +0900

    add backtracking method

--- Baekjoon/backtracking/N과 M(2).py
+++ Baekjoon/backtracking/N과 M(2).py
@@ -10,3 +10,40 @@ combs = list(map(list, combinations(nums, M)))
 
 for comb in combs:
     print(*comb)
+
+
+# 백트래킹
+# import sys
+#
+# input = sys.stdin.readline
+#
+#
+# def backtracking(list, start):
+#
+#     global answer
+#
+#     if len(list) == M:
+#         answer.append(list)
+#         return
+#
+#     for num in range(start, N + 1):
+#         if visited[num] != 1:
+#
+#             visited[num] = 1
+#             tmp = list[:]
+#             tmp.append(num)
+#
+#             backtracking(tmp, num)
+#
+#             visited[num] = 0
+#
+#
+# N, M = map(int, input().split())
+#
+# visited = [0] * (N + 1)
+# answer = []
+#
+# backtracking([], 1)
+#
+# for lst in answer:
+#     print(' '.join(map(str, lst)))
